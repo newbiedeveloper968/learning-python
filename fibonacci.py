c3acf31a7f85f11c488c90c5bd746dec8b8c6b9e
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-04-22 23:33:40 +0600

    add project fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# i am going to write a fibonacci series upto "range"
+""" 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 """
+
+upto = int(input("Fibonacci series upto: "))
+initial_series = [0,1]
+
+for _ in initial_series:
+    sumation = initial_series[-1] + initial_series[-2]
+    if sumation <= upto:
+        initial_series.append(sumation)
+    else:
+        break
+
+print(", ".join(map(str, initial_series)) + ".")
