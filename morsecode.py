82b750f2d272896f5730e51d7b9af3e807b63932
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-04-23 15:24:10 +0600

    add complete morsecode.py with standard-morse

--- morsecode.py
+++ morsecode.py
@@ -58,12 +58,12 @@ morse_code_dict = {
 
 def morse_to_standard(code):
     words = code.split(" / ")
-    decoded = []
+    standard_form = []
     for word in words:
         letters = word.split(" ")
-        code_to_letter = [morse_code_dict.get(letter, "") for letter in letters]
-        decoded.append("".join(code_to_letter))
-    return " ".join(decoded)
+        code_to_letter = [morse_code_dict.get(letter, "?") for letter in letters]
+        standard_form.append("".join(code_to_letter))
+    return " ".join(standard_form)
 
 
 invert_dict = {value: key for key, value in morse_code_dict.items()}
@@ -71,17 +71,17 @@ invert_dict = {value: key for key, value in morse_code_dict.items()}
 
 def standard_to_morse(standard):
     words = standard.upper().split(" ")
-    standard_form = []  # [.-. . --.. .-], [.- ... .... .], [-. .- .."]
+    morse_form = []
     for word in words:
-        letter_to_code = [invert_dict.get(letter) for letter in word]
-        standard_form.append(" ".join(letter_to_code))
-    return " / ".join(standard_form)
+        letter_to_code = [invert_dict.get(letter, "?") for letter in word]
+        morse_form.append(" ".join(letter_to_code))
+    return " / ".join(morse_form)
 
 
 option = input(
-    """Which one?
-1. Morse code to standard
-2. Standard to morse code\n"""
+"""1. Morse code to standard
+2. Standard to morse code
+Option: """
 )
 
 try:
