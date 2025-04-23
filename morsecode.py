089e2746de30042fc21a08dc15e82fc577413459
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-04-23 15:14:24 +0600

    add complete morsecode.py with standard-morse

--- morsecode.py
+++ morsecode.py
@@ -1,3 +1,5 @@
+import sys
+
 morse_code_dict = {
     ".-": "A",
     "-...": "B",
@@ -50,8 +52,9 @@ morse_code_dict = {
     "..--.-": "_",
     ".-..-.": '"',
     "...-..-": "$",
-    ".--.-.": "@"
+    ".--.-.": "@",
 }
+
 
 def morse_to_standard(code):
     words = code.split(" / ")
@@ -62,5 +65,31 @@ def morse_to_standard(code):
         decoded.append("".join(code_to_letter))
     return " ".join(decoded)
 
-user_input = input('Morse code: ')
-print(morse_to_standard(user_input))
+
+invert_dict = {value: key for key, value in morse_code_dict.items()}
+
+
+def standard_to_morse(standard):
+    words = standard.upper().split(" ")
+    standard_form = []  # [.-. . --.. .-], [.- ... .... .], [-. .- .."]
+    for word in words:
+        letter_to_code = [invert_dict.get(letter) for letter in word]
+        standard_form.append(" ".join(letter_to_code))
+    return " / ".join(standard_form)
+
+
+option = input(
+    """Which one?
+1. Morse code to standard
+2. Standard to morse code\n"""
+)
+
+try:
+    if int(option) == 1:
+        user_input = input("Morse code: ")
+        print(morse_to_standard(user_input))
+    elif int(option) == 2:
+        user_input = input("Standard text: ")
+        print(standard_to_morse(user_input))
+except:
+    sys.exit("Not an option!")
