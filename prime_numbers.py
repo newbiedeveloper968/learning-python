36f5265d33a212f83d2540f80d8ba490e0af59fa
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-04-26 03:57:43 +0600

    add prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,2 +1,31 @@
-""" integers > 1 with exactly two factors, 1 and the number itself. Some of the prime numbers include 2, 3, 5, 7, 11, 13 """
-""" All the prime numbers upto user input"""
+"""integers > 1 with exactly two factors, 1 and the number itself. Some of the prime numbers include 2, 3, 5, 7, 11, 13"""
+
+
+def main():
+    user_input = 20
+    prime(user_input)
+    print(prime_numbers)
+
+
+""" Checks if a single digit is a prime number or not """
+
+
+def check_prime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
+prime_numbers = []
+
+""" Appends each prime numbers to the list -> prime_numbers """
+
+
+def prime(lim):
+    for each_number in range(2, lim + 1):
+        if check_prime(each_number):
+            prime_numbers.append(each_number)
+
+
+main()
